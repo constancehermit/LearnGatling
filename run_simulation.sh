342e4902fd06f9200a6bebf9138fce49737a0f9a
Constance Armitage <constance.armitage@theartfultester.com>
2019-06-07 09:48:42 +0200

    Updates run.sh
    Adds network=host to Docker Run Gatling options

---
+++ run_simulation.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -x
+
+docker run -it --rm -v `pwd`/conf:/opt/gatling/conf -v `pwd`/user-files:/opt/gatling/user-files -v `pwd`/results:/opt/gatling/results --network="host" denvazh/gatling:3.0.3
+
