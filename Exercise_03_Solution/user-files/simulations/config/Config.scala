2059ffa0fac245cc4894ff414dcc3f477447be1a
Constance Armitage <constance.armitage@theartfultester.com>
2021-02-13 11:14:56 +0100

    Adds Exercise 3 Solution

---
+++ Exercise_03_Solution/user-files/simulations/config/Config.scala
@@ -0,0 +1,18 @@
+package config
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+object Config {
+
+    val baseURL = "http://localhost:80/api/v2/"
+
+    val httpProtocol = http
+        .baseUrl(baseURL)
+        .acceptHeader("*/*")
+        .doNotTrackHeader("1")
+        .acceptLanguageHeader("en-US,en;q=0.5")
+        .acceptEncodingHeader("gzip, deflate")
+        .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+
+}
