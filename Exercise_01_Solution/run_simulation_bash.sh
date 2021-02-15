12e90c971f25d4a75baa8a2e12663815d5f7bbeb
Constance Armitage <constance.armitage@theartfultester.com>
2021-02-15 21:41:41 +0100

    Deletes a bunch of stuff
    reset!

--- Exercise_01_Solution/run_simulation_bash.sh
+++
@@ -1,6 +0,0 @@
-docker run -it --rm \
--v `pwd`/conf:/opt/gatling/conf \
--v `pwd`/user-files:/opt/gatling/user-files \
--v `pwd`/results:/opt/gatling/results \
---network="host" \
-denvazh/gatling:3.2.1
